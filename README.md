1f5faad8961f2a424284d2ed4ff7f20cd121bc67
Géza László Szeghy <85758966+szgezal@users.noreply.github.com>
2022-05-07 17:57:30 +0200

    base/2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# 5-in-a-row
+# Widgets
 University assignment
+Made with ITK GraphicsLib
---
+++ scroll_list.hpp
@@ -0,0 +1,19 @@
+#ifndef SCROLL_LIST_HPP
+#define SCROLL_LIST_HPP
+
+#include "widget.hpp"
+
+class Scroll_list : public Widget
+{
+protected:
+    std::vector<std::string> elements;
+    int e;
+    std::string s;
+public:
+    Scroll_list(Application*, int, int, int, int, std::vector<std::string>, int);
+    void draw() override;
+    void handle(genv::event, Widget*) override;
+    std::string getElement() override;
+};
+
+#endif // SCROLL_LIST_HPP
